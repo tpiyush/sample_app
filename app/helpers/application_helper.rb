f6615d5495ae6c3c845b745a4ee5c89a432b04b7
tpiyush <piyush.tiwari@1mg.com>
2016-06-15 14:34:08 +0530

    changes added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+<<<<<<< HEAD
     # Returns the full title on a per-page basis.
       def full_title(page_title)
             base_title = "Ruby on Rails Tutorial Sample App"
@@ -8,4 +9,14 @@ module ApplicationHelper
                                     "#{base_title} | #{page_title}"
                                         end
                   end
+=======
+  def full_title(page_title)
+      base_title = "Ruby on Rails Tutorial Sample App"
+        if page_title.empty?
+              base_title
+                else
+                      "#{base_title} | #{page_title}"
+                        end
+         end
+>>>>>>> piyush
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,26 +1,38 @@
-=begin
-require 'rails_helper'
-
-RSpec.describe "StaticPages", type: :request do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      get static_pages_index_path
-      expect(response).to have_http_status(200)
-    end
-  end
-end
-=end
-
 require 'spec_helper'
 
 describe "Static pages" do
 
-    describe "Home page" do
+    subject { page }
 
-          it "should have the content 'Sample App'" do
-                  visit '/static_pages/home'
-                        expect(page).to have_content('Sample App')
+      describe "Home page" do
+            before { visit root_path }
+
+                it { should have_content('Sample App') }
+                    it { should have_title(full_title('')) }
+                        it { should_not have_title('| Home') }
+                          end
+
+        describe "Help page" do
+              before { visit help_path }
+
+                  it { should have_content('Help') }
+                      it { should have_title(full_title('Help')) }
+                        end
+
+          describe "About page" do
+                before { visit about_path }
+
+                    it { should have_content('About') }
+                        it { should have_title(full_title('About Us')) }
+                          end
+
+            describe "Contact page" do
+                  before { visit contact_path }
+
+                      it { should have_content('Contact') }
+                          it { should have_title(full_title('Contact')) }
                             end
+<<<<<<< HEAD
           it "should have the base title" do
                   visit '/static_pages/home'
                         expect(page).to have_title("Ruby on Rails Tutorial Sample App")
@@ -31,4 +43,6 @@ describe "Static pages" do
                         expect(page).not_to have_title('| Home')
                             end
           end
+=======
+>>>>>>> piyush
 end
